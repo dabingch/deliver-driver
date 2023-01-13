67a6895cfcdbd8a4b5c4732f3d19584fee7f029f
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-13 17:52:17 +1100

    Add boost speed object

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-  [SerializeField] float steerSpeed = 0.1f;
-  [SerializeField] float moveSpeed = 0.01f;
-  void Start()
-  {
-  }
+  [SerializeField] float steerSpeed = 1f;
+  [SerializeField] float moveSpeed = 20f;
+  [SerializeField] float slowSpeed = 15f;
+  [SerializeField] float boostSpeed = 30f;
 
   void Update()
   {
@@ -17,5 +16,19 @@ public class Driver : MonoBehaviour
 
     transform.Rotate(0, 0, -steerAmount);
     transform.Translate(0, moveAmount, 0);
+  }
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    moveSpeed = slowSpeed;
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Boost")
+    {
+      Debug.Log("Boost!");
+      moveSpeed = boostSpeed;
+    }
   }
 }
