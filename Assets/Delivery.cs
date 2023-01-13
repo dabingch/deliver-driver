c840166e2824096d0173d34f052632a3038f1dd7
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-13 17:24:12 +1100

    Add destroy object function

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+  [SerializeField] float destroyDelay = 0.5f;
+  bool hasPackage;
   void OnCollisionEnter2D(Collision2D other)
   {
     Debug.Log("Collision!");
@@ -11,13 +13,16 @@ public class Delivery : MonoBehaviour
 
   void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.tag == "Package")
+    if (other.tag == "Package" && !hasPackage)
     {
       Debug.Log("Package pick up!");
+      hasPackage = true;
+      Destroy(other.gameObject, destroyDelay);
     }
 
-    if (other.tag == "Customer")
+    if (other.tag == "Customer" && hasPackage)
     {
+      hasPackage = false;
       Debug.Log("Package delivered!");
     }
   }
