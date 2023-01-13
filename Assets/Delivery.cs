272ef9fdf3977a5643e04299f12b299d0ca7d465
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-13 17:13:19 +1100

    Add package and customer

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
   void OnCollisionEnter2D(Collision2D other)
   {
@@ -11,6 +11,14 @@ public class Collision : MonoBehaviour
 
   void OnTriggerEnter2D(Collider2D other)
   {
-    Debug.Log("Trigger!");
+    if (other.tag == "Package")
+    {
+      Debug.Log("Package pick up!");
+    }
+
+    if (other.tag == "Customer")
+    {
+      Debug.Log("Package delivered!");
+    }
   }
 }
